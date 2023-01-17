d64179e4b5bae5c1b2f00d0b369b2f26ce78735e
Thomas Felipe de Faria Godoi <thomas.godoi@iacit.com.br>
2023-01-17 20:35:50 -0300

    Creating pills

--- src/Components/LeftButton/LeftButton.js
+++ src/Components/LeftButton/LeftButton.js
@@ -4,7 +4,6 @@ import { PerkDataService } from "../../Services/PerkDataService";
 import './LeftButton.css';
 
 export default function LeftButton() {
-    // const [leftButton, setLeftButton] = useState(null);
     const [perks, setPerks] = useState([]);
 
     async function getPerk() {
@@ -18,17 +17,52 @@ export default function LeftButton() {
         getPerk();
     }, [])
 
+    const cyanShadow = "0 0 3px rgb(0, 255, 255, 0.8)";
+    const redShadow = "0 0 3px rgb(255, 0, 0, 0.8)";
+
+    function handleTierColor(tier) {
+        switch(tier) {
+            case "S+":
+                return "gold"
+
+            case "S" || "A" || "B" || "C":
+                return "white"
+
+            case "D" || "E":
+                return "grey"
+        }
+    }
+
+    function handleTierShadow(tier) {
+        const goldShadow = "0 0 3px rgb(255, 215, 0, 0.8)";
+        const whiteShadow = "0 0 3px rgb(255, 255, 255, 0.8)";
+        const silvershadow = "0 0 3px rgb(192, 192, 192, 0.8)";
+
+
+        switch(tier) {
+            case "S+":
+                return goldShadow;
+
+            case "S" || "A" || "B" || "C":
+                return whiteShadow;
+
+            case "D" || "E":
+                return silvershadow;
+        }
+    }
+
     return (
         <div style={{fontSize:'12px', textAlign:'initial'}}>
 
             {perks.map((el, key) => 
-                <div className="perk-list" key={key} style={{color: el.isPositive ? "cyan" : "indianred"}}>
+                <div className="perk-list" key={key} style={{ color: el.isPositive ? "cyan" : "red", textShadow: el.isPositive ? cyanShadow : redShadow }}>
                     <span className="description">{el.description}</span>
-                    <span className="tier">{el.tier}</span>
+                    <span className="tier" style={{color: handleTierColor(el.tier), textShadow: handleTierShadow(el.tier)}}>{el.tier}</span>
                 </div>
             )}
 
-            <button>show log</button>
+            <button className="option1-btn">show log</button>
+            <button className="option2-btn">show log</button>
         </div>
     )
 
